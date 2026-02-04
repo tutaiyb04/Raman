479028dbb11f9e1acd783fcc7482d5fa9b8c8d05
Tú Tài <tutaiyb2411@gmail.com>
2026-02-05 02:40:12 +0700

    app.py & run_app.py

--- run_app.py
+++ run_app.py
@@ -1,18 +1,18 @@
 import streamlit.web.cli as stcli
 import os, sys
 
-def resolve_path(path):
-    # Hàm xử lý đường dẫn khi đóng gói vào file .exe
-    basedir = getattr(sys, '_MEIPASS', os.getcwd())
-    return os.path.join(basedir, path)
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
 
 if __name__ == "__main__":
-    # Kích hoạt Streamlit để chạy giao diện chính của bạn
-    # Thay 'app.py' bằng tên file giao diện bạn đang sử dụng
     sys.argv = [
         "streamlit",
         "run",
-        resolve_path("app.py"),
+        resource_path("app.py"), # Thay bằng tên file giao diện của bạn
         "--global.developmentMode=false",
     ]
     sys.exit(stcli.main())
